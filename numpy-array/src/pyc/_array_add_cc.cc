26785bca16ef7c8324e08b82fb2ba3507b1ec746
Ahn, Seongjin <jam45@naver.com>
2018-07-14 12:44:28 +0000

    cleanup code for _array_add_cc module

--- numpy-array/src/pyc/_array_add_cc.cc
+++ numpy-array/src/pyc/_array_add_cc.cc
@@ -2,7 +2,6 @@
 #include <numpy/arrayobject.h>
 #include "array_add.hh"
 #include <complex>
-#include <iostream>
 
 extern "C" { static PyObject *_array_add_cc_add_array_general(PyObject *self, PyObject *args); }
 
@@ -18,13 +17,10 @@ static PyModuleDef _array_add_cc {
   module_methods
 };
 
-
 extern "C" PyMODINIT_FUNC PyInit__array_add_cc(void) {
-  std::cout << "in init function\n";
 	import_array();
 	return PyModule_Create(&_array_add_cc);
 }
-
 
 template <class T>
 int add_array_core_template(PyObject *a_array, PyObject *b_array, PyObject *out_array, long N) {
@@ -37,28 +33,18 @@ int add_array_core_template(PyObject *a_array, PyObject *b_array, PyObject *out_
       PyErr_SetString(PyExc_Exception, "Failed to add array");
       return 1;
     }
-
     return 0;
 }
 
 
-// template instantiation
-//template int add_array_core_template<double>(PyObject *, PyObject *, PyObject *, long);
-//template int add_array_core_template<std::complex<double>>(PyObject *, PyObject *, PyObject *, long);
-
-
 static PyObject *_array_add_cc_add_array_general(PyObject *self, PyObject *args) {
 
-  std::cout << "At the head of _array_add_cc_add_array_general()\n";
-  
 	PyObject *a_obj, *b_obj, *out_obj;
 	long N;
 
 	if (!PyArg_ParseTuple(args, "OOO", &a_obj, &b_obj, &out_obj)) { 
 		PyErr_SetString(PyExc_Exception, "Failed to parse argument tuple");
 		return NULL; }
-
-  std::cout << "After parsing at _array_add_cc_add_array_general\n";
 
 	PyObject *a_array = PyArray_FROM_OTF(a_obj, NPY_NOTYPE, NPY_IN_ARRAY);
 	PyObject *b_array = PyArray_FROM_OTF(b_obj, NPY_NOTYPE, NPY_IN_ARRAY);
@@ -72,7 +58,7 @@ static PyObject *_array_add_cc_add_array_general(PyObject *self, PyObject *args)
 		return NULL;
 	}
 
-  std::cout << "After getting *_array objects\n";
+
 
 	N = (long) PyArray_DIM(a_array, 0);
 
@@ -84,35 +70,18 @@ static PyObject *_array_add_cc_add_array_general(PyObject *self, PyObject *args)
     goto fail;
   } else { typenum = typenum_a; }
 
-  std::cout << "After typenum check\n";
+
   
   if (typenum == NPY_DOUBLE) {
     if (add_array_core_template<double>(a_array, b_array, out_array, N) != 0) { goto fail; }
-    
-//  	double *a = (double *) PyArray_DATA(a_array);
-//	  double *b = (double *) PyArray_DATA(b_array);
-//  	double *out = (double *) PyArray_DATA(out_array);
-//	
-//  	if (add_array_type_double(a, b, out, N) != 0) {
-//      PyErr_SetString(PyExc_Exception, "Failed to run `add_array_type_double()`");
-//      goto fail;
-//    }
   } else if (typenum == NPY_COMPLEX128) {
     if (add_array_core_template<std::complex<double>>(a_array, b_array, out_array, N) != 0) { goto fail; }
-//    std::complex<double> *a = (std::complex<double> *) PyArray_DATA(a_array);
-//    std::complex<double> *b = (std::complex<double> *) PyArray_DATA(b_array);
-//    std::complex<double> *out = (std::complex<double> *) PyArray_DATA(out_array);
-//
-//  	if (add_array_type_complex(a, b, out, N) != 0) {
-//      PyErr_SetString(PyExc_Exception, "Failed to run `add_array_type_complex()`");
-//      goto fail;
-//    }
   } else {
     PyErr_SetString(PyExc_Exception, "Unexpected typenum");
     goto fail;
   }
 
-  std::cout << "After add_array_core_template()\n";
+
 
   Py_INCREF(Py_None);
 
@@ -120,9 +89,7 @@ static PyObject *_array_add_cc_add_array_general(PyObject *self, PyObject *args)
 	Py_DECREF(b_array);
 	Py_DECREF(out_array);
 
-	//return ret;
 	return Py_None;
-
 
 fail:
 	Py_XDECREF(a_array);
@@ -130,5 +97,4 @@ fail:
 	Py_XDECREF(out_array);
 	return NULL;
 }
-
 
